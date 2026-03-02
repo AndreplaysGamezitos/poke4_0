881b8bfa8aa22c0b4a13da99224c0e726dae39fd
AndreplaysGamezitos <amdreyoung@gmail.com>
2026-03-01 22:19:47 -0300

    Delete Reference directory

--- Reference/database/migration_avatar.sql
+++
@@ -1,4 +0,0 @@
--- Migration: Add avatar_id to accounts table
--- Run this on your database to add persistent avatar selection to accounts
-
-ALTER TABLE accounts ADD COLUMN IF NOT EXISTS avatar_id INT DEFAULT 1 AFTER account_code;
